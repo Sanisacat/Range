f151e13075a152f7b4e7620744e3884c83e53238
Sanchit Dhauchak <86495146+Sanisacat@users.noreply.github.com>
2021-10-05 15:57:27 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,6 @@
+print("This is a program to generate multiplication tables ")
+n=int(input("Enter the number for printing out its table u sucka: "))
+
+
+for i in range(1,11):
+    print(n," x ",i," = ", i*n)
